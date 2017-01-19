7da30882d0be9917cb6e182014780d996aee98ec
Unknown <mahmoodgladney@gmail.com>
2017-01-18 18:53:08 -0700

    Begin writing code for motors

--- src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsystem.java
@@ -3,15 +3,25 @@ package org.usfirst.frc.team4183.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.Encoder; 
+import edu.wpi.first.wpilibj.RobotDrive;
 /**
  *
  */
 public class DriveSubsystem extends Subsystem {
-
-    // Put methods for controlling this subsystem
+		private CANTalon leftMotor0;
+		private CANTalon leftMotor1; 
+		private CANTalon rightMotor0;
+		private CANTalon rightMotor1;
+	    
+    
+		
+	// Put methods for controlling this subsystem
     // here. Call these from Commands.
-
-    public void initDefaultCommand() {
+public DriveSubsystem() {
+	RobotDrive = new RobotDrive(leftMotor0, leftMotor1, rightMotor0, rightMotor1);
+}	
+	
+public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
