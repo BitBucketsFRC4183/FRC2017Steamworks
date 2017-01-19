2e54a86af0ee60cf1cb0af70632767a3d1a62e46
Unknown <mahmoodgladney@gmail.com>
2017-01-18 17:56:02 -0700

    Drive
    Added drive subsystem and removed examples command and subsystem

---
+++ src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsytem.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team4183.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.Encoder; 
+/**
+ *
+ */
+public class DriveSubsytem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
