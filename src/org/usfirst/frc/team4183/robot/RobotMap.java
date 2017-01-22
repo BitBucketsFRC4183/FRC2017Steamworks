73728665778a5d221cc181b3d970efbd2bbb4ddf
ECarrasco38799 <ECarrasco3879@aol.com>
2017-01-22 13:30:40 -0700

    created ball manipulator subsystem
    created basic ballManip subsystem with variables and integrated into robotMap

--- src/org/usfirst/frc/team4183/robot/RobotMap.java
+++ src/org/usfirst/frc/team4183/robot/RobotMap.java
@@ -14,11 +14,33 @@ public class RobotMap {
 	public static int leftMotor1 = 1;
 	public static int rightMotor0 = 3;
 	public static int rightMotor1 = 4; 
+	
 	public static final CANTalon.FeedbackDevice DRIVE_ENCODER1 = CANTalon.FeedbackDevice.QuadEncoder;
 	public static final int DRIVE_PULSES_PER_REV = 256; 
 	public static final CANTalon.FeedbackDevice DRIVE_ENCODER2 = CANTalon.FeedbackDevice.QuadEncoder;
 	public static final int DRIVE_PULSES_PER_REV2 = 256; 
+	
 	public static int climbMotor = 5;
+	
+	
+	
+	
+//stuff for the BALL MANIPULATOR is below here
+	public static int shooterMotor = 9; 
+	public static int vertiMotor = 10;
+	public static int horizMotor = 11;
+	
+	public static final CANTalon.FeedbackDevice SHOOTER_ROLLER_ENCODER = CANTalon.FeedbackDevice.QuadEncoder;
+	public static final CANTalon.FeedbackDevice VERTICAL_CONVEYER_ENCODER = CANTalon.FeedbackDevice.QuadEncoder;
+	public static final CANTalon.FeedbackDevice HORIZONTAL_CONVEYER_ENCODER = CANTalon.FeedbackDevice.QuadEncoder;
+	
+	public static final int SHOOTER_ROLLER_PULSES_PER_REV = 256;
+	public static final int VERTI_CONV_PULSES_PER_REV = 256;
+	public static final int HORIZ_CONV_PULSES_PER_REV = 256;
+	
+	
+	
+	
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
 	// public static int leftMotor = 1;
---
+++ src/org/usfirst/frc/team4183/robot/subsystems/BallManipSubsystem.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team4183.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.CANTalon;
+import org.usfirst.frc.team4183.robot.RobotMap;
+
+
+public class BallManipSubsystem extends Subsystem {
+	
+	boolean isShooting;
+	
+	private CANTalon shooterMotor;
+	private CANTalon conveyerVerti;
+	private CANTalon conveyerHoriz;
+	
+	double shooterVel;
+	double vertiVel;
+	double horizVel;
+	
+	
+	public BallManipSubsystem(){
+		shooterMotor = new CANTalon(RobotMap.shooterMotor);
+		conveyerVerti = new CANTalon(RobotMap.vertiMotor);
+		conveyerHoriz = new CANTalon(RobotMap.horizMotor);
+		
+	}
+	
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
