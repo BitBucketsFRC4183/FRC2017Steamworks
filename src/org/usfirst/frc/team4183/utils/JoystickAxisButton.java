e7bb6f45b7628858ad0fdccd4e9ba4039bee706d
Tim Wilson <timwilson235@gmail.com>
2017-01-24 20:46:52 -0700

    Removed unused import

--- src/org/usfirst/frc/team4183/utils/JoystickAxisButton.java
+++ src/org/usfirst/frc/team4183/utils/JoystickAxisButton.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team4183.utils;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 public class JoystickAxisButton extends Button {
