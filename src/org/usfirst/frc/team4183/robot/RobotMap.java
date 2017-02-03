c141375e4510b58e64c11a707d6014db4257b519
Tim Wilson <timwilson235@gmail.com>
2017-02-03 14:06:07 -0700

    Assign bogus (but non-colliding) motor nums

--- src/org/usfirst/frc/team4183/robot/RobotMap.java
+++ src/org/usfirst/frc/team4183/robot/RobotMap.java
@@ -10,39 +10,35 @@ import com.ctre.CANTalon;
  */
 public class RobotMap {
 
+	// tjw:
+	// Current proto board has only 4 controllers;
+	// until we get that fixed, I've assigned fantastic motor numbers in here
+	// to make sure there's no collisions.
+	// When testing a particular subsystem, put real numbers in here,
+	// build & download, but DON'T commit that version.
+	
 	// Drive Subsystem
-	public static final int LEFT_MOTOR0_ID = 0;
-	public static final int LEFT_MOTOR1_ID = 1;
-	public static final int RIGHT_MOTOR0_ID = 3;
-	public static final int RIGHT_MOTOR1_ID = 4; 
+	public static final int LEFT_MOTOR0_ID = 20;
+	public static final int LEFT_MOTOR1_ID = 21;
+	public static final int RIGHT_MOTOR0_ID = 22;
+	public static final int RIGHT_MOTOR1_ID = 23; 
 	
 	public static final CANTalon.FeedbackDevice DRIVE_ENCODER = CANTalon.FeedbackDevice.QuadEncoder;
 	public static final int DRIVE_PULSES_PER_REV = 2048; 
 	
 	
 	// Climb Subsystem
-	public static final int CLIMB_MOTOR = 21;
-	
-	
+	public static final int CLIMB_MOTOR = 31;
+		
 
-	//stuff for the BALL MANIPULATOR is below here
-	public static final int BALL_SUBSYSTEM_TOP_ROLLER_MOTOR_ID = 9; 
-	public static final int BALL_SUBSYSTEM_CONVEYER_MOTOR_ID = 10;
-	public static final int BALL_SUBSYSTEM_SWEEPER_MOTOR_ID = 2;
-	
+	// Ball Manipulator
+	public static final int BALL_SUBSYSTEM_TOP_ROLLER_MOTOR_ID = 40; 
+	public static final int BALL_SUBSYSTEM_CONVEYER_MOTOR_ID = 41;
+	public static final int BALL_SUBSYSTEM_SWEEPER_MOTOR_ID = 42;
 	
 	public static final int SHOOTER_ROLLER_PULSES_PER_REV = 256;
 	
 	
-	
-	
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	// Gear Handler
+	public static final int GEAR_HANDLER_MOTOR = 51;
 }
