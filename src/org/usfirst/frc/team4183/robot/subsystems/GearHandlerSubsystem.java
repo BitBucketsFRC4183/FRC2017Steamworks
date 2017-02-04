ce840dd82a21872b1c6a09430bf5b008c37f23c4
ECarrasco38799 <ECarrasco3879@aol.com>
2017-02-03 18:29:42 -0700

    PNEUMATICS!!!
    Pneumatics integrated into ballManip and gear subsystems. Used DoubleSolenoids.

--- src/org/usfirst/frc/team4183/robot/subsystems/GearHandlerSubsystem.java
+++ src/org/usfirst/frc/team4183/robot/subsystems/GearHandlerSubsystem.java
@@ -1,18 +1,23 @@
 package org.usfirst.frc.team4183.robot.subsystems;
 
 
+import org.usfirst.frc.team4183.robot.RobotMap;
 import org.usfirst.frc.team4183.robot.commands.GearHandlerSubsystem.Idle;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class GearHandlerSubsystem extends Subsystem {
-
+	
+	DoubleSolenoid gearGateSolenoid = new DoubleSolenoid(RobotMap.GEAR_HANDLER_PNEUMA_OPEN_CHANNEL, RobotMap.GEAR_HANDLER_PNEUMA_CLOSED_CHANNEL); 
+	
 	public void enable() {
 	}
 	
 	public void disable() {
 		stopRoller();
 		closeGate();
+		gearGateSolenoid.set(DoubleSolenoid.Value.kOff);
 	}
 	
 	public void initDefaultCommand() {
@@ -20,11 +25,11 @@ public class GearHandlerSubsystem extends Subsystem {
 	}
 	
 	public void openGate() {
-		
+		gearGateSolenoid.set(DoubleSolenoid.Value.kForward);
 	}
 	
 	public void closeGate() {
-		
+		gearGateSolenoid.set(DoubleSolenoid.Value.kReverse);
 	}
 	
 	public void spinRollerBalls() {
