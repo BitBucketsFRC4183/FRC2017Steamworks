f660898d7f71c3ea1ffb8ed84bb4d9ad4e33bc98
Joseph Acevedo <josephjjrf@hotmail.com>
2017-02-03 17:31:50 -0700

    Filled in some methods for GearHandlingSubsystem

--- src/org/usfirst/frc/team4183/robot/subsystems/GearHandlerSubsystem.java
+++ src/org/usfirst/frc/team4183/robot/subsystems/GearHandlerSubsystem.java
@@ -1,13 +1,20 @@
 package org.usfirst.frc.team4183.robot.subsystems;
 
 
+import org.usfirst.frc.team4183.robot.RobotMap;
 import org.usfirst.frc.team4183.robot.commands.GearHandlerSubsystem.Idle;
+
+import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class GearHandlerSubsystem extends Subsystem {
-
+	
+	private final CANTalon BALL_GEAR_INTAKE_MOTOR = new CANTalon(RobotMap.GEAR_HANDLER_MOTOR);
+	private static final double MOTOR_SPEED = 1.0;
+	
 	public void enable() {
+		
 	}
 	
 	public void disable() {
@@ -28,15 +35,15 @@ public class GearHandlerSubsystem extends Subsystem {
 	}
 	
 	public void spinRollerBalls() {
-		
+		BALL_GEAR_INTAKE_MOTOR.set(MOTOR_SPEED);
 	}
 	
 	public void spinRollerGear() {
-		
+		BALL_GEAR_INTAKE_MOTOR.set(-MOTOR_SPEED);
 	}
 	
 	public void stopRoller() {
-		
+		BALL_GEAR_INTAKE_MOTOR.set(0);
 	}
 	
 }
