9164bc4f43a615548d28d75c244a45c8e27e073f
Tim Wilson <timwilson235@gmail.com>
2017-02-10 08:14:29 -0700

    Serial port test in Manager

---
+++ src/org/usfirst/frc/team4183/utils/SerialPortManager.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team4183.utils;
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+public class SerialPortManager {
+	
+	public interface PortTester {
+		boolean test( String input);
+	}
+	
+	public static SerialPort findPort( PortTester tester, int baudRate) {
+		return findPort( tester, baudRate, 2000);
+	}
+
+	public static SerialPort findPort( PortTester tester, int baudRate, int msecs) {
+		
+		String[] portNames = SerialPortList.getPortNames();
+
+		System.out.print("Port list:");
+		for(String portName : portNames)
+			System.out.print(" " + portName);
+		System.out.println();		
+		
+		for( String portName : portNames) {
+			
+			try {
+				
+				System.out.println("Trying port:" + portName);
+				SerialPort port = new SerialPort(portName);
+				port.openPort();
+
+				port.setParams(SerialPort.BAUDRATE_115200,
+						SerialPort.DATABITS_8, 
+						SerialPort.STOPBITS_1, 
+						SerialPort.PARITY_NONE);
+				
+
+				String inBuff = "", inStr;				
+				long tQuit = System.currentTimeMillis() + msecs;
+				while( System.currentTimeMillis() < tQuit) {
+					
+					// Get input & append to inBuff
+					if( (inStr = port.readString()) != null) {
+						inBuff += inStr;
+					}
+					
+					if( tester.test(inBuff) ) {
+						System.out.format( "Success on port %s", portName);
+						return port;
+					}
+				}
+				
+				port.closePort();				
+			}
+			catch( SerialPortException ex) {}			
+		}
+		
+		// Not found
+		return null;
+	}
+
+}
