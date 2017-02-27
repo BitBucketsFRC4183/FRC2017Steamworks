59879a0e039ba424f5324356f9e53b86a3d837fb
mtkessel <mtkessel@mac.com>
2017-02-27 07:22:57 -0700

    Color on boiler magic
    Bounding box gets greener as we get closer to more matching points; minimum points is red

--- BucketVision/matchBoiler.py
+++ BucketVision/matchBoiler.py
@@ -36,6 +36,8 @@ SOFTWARE.
 # import the necessary packages
 import numpy as np
 import cv2
+import time
+import math
 #from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
@@ -43,10 +45,16 @@ MIN_MATCH_COUNT = 10
 # load the image, convert it to grayscale, and detect edges
 img1 = cv2.imread("redBoilerTrainWhole.jpg",cv2.IMREAD_GRAYSCALE)
 #img1 = cv2.resize(img1,(320,240))
+#img1 = cv2.resize(img1,(int(img1.shape[1]/2),int(img1.shape[0]/2)))
+print(img1.shape)
 
-img2 = cv2.imread('blueBoiler11ftLeft.jpg',cv2.IMREAD_GRAYSCALE) 
-img2 = cv2.resize(img2,(320,240))
+img2c = cv2.imread('redBoiler14ftRight.jpg')
+img2c = cv2.resize(img2c,(320,240))
+#img2c = cv2.resize(img2c,(int(img2c.shape[1]/3),int(img2c.shape[0]/3)))
+img2 = cv2.cvtColor(img2c, cv2.COLOR_BGR2GRAY)
 
+
+starttime = time.time()
 
 # Initiate SIFT detector
 sift = cv2.xfeatures2d.SIFT_create()
@@ -98,8 +106,9 @@ if (crossCheck == False):
     for m,n in matches:
         if m.distance < 0.7*n.distance:
             good.append(m)
-            
-    if (len(good)>MIN_MATCH_COUNT):
+    
+    goodCount= len(good)
+    if (goodCount>MIN_MATCH_COUNT):
         src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
@@ -107,7 +116,17 @@ if (crossCheck == False):
         h,w = img1.shape
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
         dst = cv2.perspectiveTransform(pts,M)
-        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,2, cv2.LINE_AA)
+        
+        
+        angle = (goodCount-MIN_MATCH_COUNT-1)
+        if (angle > 50):
+            angle = 50
+        
+        angle = math.pi/2 * (angle / 50)
+        r = int(math.cos(angle)*255)
+        g = int(math.sin(angle)*255)
+        
+        img2 = cv2.polylines(img2c,[np.int32(dst)],True,(0,g,r),2, cv2.LINE_AA)
     else:
         print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
         matchesMask = None
@@ -124,7 +143,9 @@ else:
     ## Draw first 10 matches.
     img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], None, flags=2)
 
+endtime = time.time()
 
+print(endtime - starttime)
 cv2.imshow("Image", img3)
 cv2.waitKey(0)
 
